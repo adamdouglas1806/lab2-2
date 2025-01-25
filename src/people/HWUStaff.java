8a9cdc8e9d976edf20d2659810b0a9640ab68b33
Swaraj Dash <swaraj.dash@hw.ac.uk>
2025-01-25 18:37:22 +0000

    starter code

---
+++ src/people/HWUStaff.java
@@ -0,0 +1,6 @@
+package people;
+
+public class HWUStaff {
+
+	private static String[] offices = {"EMG.12", "EMG.13", "EMG.14", "EMG.15", "EM1.23", "EM1.24", "EM1.25", "EM1.26", "EM2.34", "EM2.35", "EM2.36", "EM2.37"};
+}
---
+++ src/people/Run.java
@@ -0,0 +1,25 @@
+package people;
+
+public class Run {
+
+	public static void main(String[] args) {
+		// STEP 1 - Add the appropriate code in the other files
+		
+
+		// STEP 2a - Leave the print statement for ease of marking
+		System.out.println("Step 2a:");
+		// YOUR STEP 2a CODE GOES HERE!
+		
+				
+		// STEP 2b - Leave the print statement for ease of marking
+		System.out.println("Step 2b:");
+		// YOUR STEP 2b CODE GOES HERE!
+		
+		
+		// STEP 3 - Leave the print statement for ease of marking
+		System.out.println("Step 3:");
+		// YOUR STEP 3 CODE GOES HERE!
+		
+	}
+
+}
